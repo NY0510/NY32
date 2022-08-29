43acb2e1597026471c77f97807dbd17003b9e657
NY64 <me@ny64.kr>
2022-08-29 21:59:18 +0900

    텍스트 수정

--- commands/ipinfo.js
+++ commands/ipinfo.js
@@ -6,8 +6,8 @@ const fetch = require("node-fetch");
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("ipinfo")
-		.setDescription("📡 IP 정보 조회")
-		.addStringOption(option => option.setName("ip_adress").setDescription("조회할 IP주소를 입력하세요").setRequired(true)),
+		.setDescription("📡 IP 정보를 조회합니다")
+		.addStringOption(option => option.setName("ip_adress").setDescription("조회할 IP주소").setRequired(true)),
 
 	async execute(interaction) {
 		const inputIp = interaction.options.getString("ip_adress");
@@ -19,7 +19,7 @@ module.exports = {
 				/^^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$/
 			)
 		) {
-			const e = new EmbedBuilder().setDescription(`${process.env.EMOJI_X} **${inputIp}**는 유효한 IP가 아닙니다`).setColor(0x212326);
+			const e = new EmbedBuilder().setDescription(`${process.env.EMOJI_X} **${inputIp}**은(는) 유효한 IP가 아닙니다`).setColor(0x212326);
 			return await interaction.reply({ embeds: [e], ephemeral: true });
 		}
 
@@ -28,7 +28,7 @@ module.exports = {
 
 		// 조회된 IP 정보 없을때
 		if (json.country == undefined && json.country == undefined && json.region == undefined && json.city == undefined && json.org == undefined && json.timezone == undefined) {
-			const e = new EmbedBuilder().setDescription(`${process.env.EMOJI_X} **${inputIp}**는 유효한 IP가 아닙니다`).setColor(0x212326);
+			const e = new EmbedBuilder().setDescription(`${process.env.EMOJI_X} **${inputIp}**은(는) 유효한 IP가 아닙니다`).setColor(0x212326);
 			return await interaction.reply({ embeds: [e], ephemeral: true });
 		}
 
@@ -40,35 +40,30 @@ module.exports = {
 					value: `┕**\`${json.ip}\`**`,
 					inline: true,
 				},
-
 				{
 					name: "🌏 국가",
 					value: `┕**\`${json.country}\`**`,
 
 					inline: true,
 				},
-
 				{
 					name: "🗺️ 지역",
 					value: `┕**\`${json.region} ${json.city}\`**`,
 
 					inline: true,
 				},
-
 				{
 					name: "📍 좌표",
 					value: `┕**\`${json.loc.replace(",", ", ")}\`**`,
 
 					inline: true,
 				},
-
 				{
 					name: "📡 ISP",
 					value: `┕**\`${json.org}\`**`,
 
 					inline: true,
 				},
-
 				{
 					name: "🕐 타임존",
 					value: `┕**\`${json.timezone}\`**`,
