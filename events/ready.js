401f25bf04f52f3cd6bcdfc555ee13566a9e666c
NY64 <me@ny64.kr>
2022-08-29 22:10:57 +0900

    유저 카운트 버그 수정

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,15 @@ module.exports = {
 	name: "ready",
 	once: true,
 	execute(client) {
+		let userCount = 0;
+
+		client.guilds.cache.forEach(guild => {
+			guild.members.cache.forEach(member => {
+				userCount++;
+			});
+		});
+
 		console.log(`Logged in as ${client.user.tag} (${client.user.id})`);
-		console.log(`In ${client.guilds.cache.size} guilds, ${client.users.cache.size} users\n`);
+		console.log(`In ${client.guilds.cache.size} guilds, ${userCount} users\n`);
 	},
 };
